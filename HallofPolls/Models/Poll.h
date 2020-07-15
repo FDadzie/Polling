578fb681546ced0e092f17a041d3b98c9339d926
Frank E. Dadzie <fdadzie20@fb.com>
2020-07-14 20:16:06 -0400

    Added Poll Model

---
+++ HallofPolls/Models/Poll.h
@@ -0,0 +1,23 @@
+//
+//  Poll.h
+//  HallofPolls
+//
+//  Created by fdadzie20 on 7/14/20.
+//  Copyright © 2020 fdadzie20. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Poll : PFObject<PFSubclassing>
+
+@property (nonatomic, strong) NSNumber *totalVoteCount;
+@property (nonatomic, strong) NSNumber *optionCount;
+@property (nonatomic) Boolean *isOpen;
+@property (nonatomic, strong) NSString *option;
+@property (nonatomic, strong) NSString *pollQuestion;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HallofPolls/Models/Poll.m
@@ -0,0 +1,13 @@
+//
+//  Poll.m
+//  HallofPolls
+//
+//  Created by fdadzie20 on 7/14/20.
+//  Copyright © 2020 fdadzie20. All rights reserved.
+//
+
+#import "Poll.h"
+
+@implementation Poll
+
+@end
